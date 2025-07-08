ae145541b1bd0153b7d0a7f6c242408f7ad82515
Kate Moreland <142517368+kmoreland126@users.noreply.github.com>
2025-07-08 09:47:39 -0500

    Add files via upload

---
+++ Assignment 3/Assignment 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment 3
+//
+//  Created by Kate Moreland on 6/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
